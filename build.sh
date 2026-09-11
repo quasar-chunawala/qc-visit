d962b66e1336a89da5aefb299985ce3541f42340
quasar-chunawala <quasar.chunawala@gmail.com>
2026-09-11 17:14:46 +0100

    update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,12 @@
 # from the qc-visit root (where you ran `ls -R`)
 
-# libstdc++ builds
-clang++ -std=c++23 -O3 -DNDEBUG -stdlib=libstdc++ -Icore \
-    core/tests_visit_libcpp.cpp -lbenchmark -lpthread \
-    -o bench_libcpp_libstdcxx
+# gc++ builds
 
-clang++ -std=c++23 -O3 -DNDEBUG -stdlib=libstdc++ -Icore \
-    core/tests_visit_polyarray.cpp -lbenchmark -lpthread \
-    -o bench_polyarray_libstdcxx
+g++ -std=c++23 -O2 -DNDEBUG -Icore core/tests_visit_flat_array.cpp -lbenchmark -lpthread -o bench_flat_array
+./bench_flat_array --benchmark_min_time=1.0s --benchmark_repetitions=20
 
-clang++ -std=c++23 -O3 -DNDEBUG -stdlib=libstdc++ -Icore \
-    core/tests_visit_flat_array.cpp -lbenchmark -lpthread \
-    -o bench_flat_array_libstdcxx
+g++ -std=c++23 -O2 -DNDEBUG -Icore core/tests_visit_polyarray.cpp -lbenchmark -lpthread -o bench_polyarray_gcc
+./bench_polyarray_gcc --benchmark_min_time=1.0s --benchmark_repetitions=20
+
+g++ -std=c++23 -O2 -DNDEBUG -Icore core/tests_visit_stdlib.cpp -lbenchmark -lpthread -o bench_stdlib_gcc
+./bench_stdlib_gcc --benchmark_min_time=1.0s --benchmark_repetitions=20
